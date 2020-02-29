f75a51db18fa76e33ce7f37206693b35bf573ca6
Zoe Ames <zoeames@gmail.com>
2020-02-29 10:34:34 -0600

    shark tank displays

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,7 +1,20 @@
 import utils from '../helpers/utils.js';
+import personData from '../helpers/data/personData.js';
 
 const tankBuilder = () => {
-  let domString = 'help 2.0';
+  const persons = personData.getAlivePersons();
+  let domString = '';
+  domString += '<div class="card">';
+  domString += '<div class="card-header">SHARK TANK</div>';
+  domString += '<ul class="list-group list-group-flush">';
+
+  persons.forEach((person) => {
+    domString += `<li class="list-group-item">${person.name}</li>`;
+  });
+
+  domString += '</ul>';
+  domString += '</div>';
+
   utils.printToDom('tank', domString);
 };
 
